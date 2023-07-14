cb04929dd11e9d2d1052a99e16023817874c1b24
David_Reyes <jd.reyes@duocuc.cl>
2023-07-14 04:12:00 -0400

    commitf

--- plant_keeper/plant_keeper/urls.py
+++ plant_keeper/plant_keeper/urls.py
@@ -8,11 +8,5 @@ from appPK import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('galeria', views.galeria, name='galeria'),
-    path('calendario/', views.calendario, name='calendario'),
-    path('foro/', views.foro, name='foro'),
-    path('contacto/', views.contacto, name='contacto'),
     path('',include('appPK.urls')),
-
 ] 
